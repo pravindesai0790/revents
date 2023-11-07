99b11efab9ee7c801e1ccbd2c2da60ff98fae6d1
pravindesai0790 <pravindesai0790@gmail.com>
2023-11-08 00:22:23 +0530

    Created event dashboard and nav bar

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,8 +1,16 @@
+import { Container } from "semantic-ui-react"
+import EventDashboard from "../../features/events/dashboard/EventDashboard"
+import NavBar from "./nav/NavBar"
 
 function App() {
 
   return (
-    <h1>Welcome to Revent</h1>
+    <>
+      <NavBar />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+    </>
   )
 }
 
